6ecc00a7fc7cf768e62ae4f47e51e40d50d0e298
Farney Jiménez <farney9@gmail.com>
2024-03-05 11:49:02 -0500

    add new todo project

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+}
